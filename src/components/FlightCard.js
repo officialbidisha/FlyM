fd715a56b7b7a79e0c16641f743246b1f0de6543
Bidisha Das <officialbidisha1@gmail.com>
2022-03-13 00:51:40 +0530

    Flight app.

---
+++ src/components/FlightCard.js
@@ -0,0 +1,8 @@
+const FlightCard = (flightConfiguration) => {
+    const { airline, departiure, arrival, duration, price } = flightConfiguration;
+    // return (
+
+    // )
+}
+
+export default FlightCard;
