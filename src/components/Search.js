20fdfd7563a3c1841efe97f9b6d7da0f08c02a85
Bidisha Das <officialbidisha1@gmail.com>
2022-04-27 01:42:17 +0530

    Need to add date formatter

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,10 +8,20 @@ import { useDispatch } from "react-redux";
 import { dateFormatter } from "../utility/dateFormatter";
 import { useNavigate } from "react-router-dom";
 import "./Search.scss";
+import axios from "axios";
+import flying from "./../assets/images/flying.png";
+
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 const Search = () => {
   const [fromLandmark, setFromLandmark] = useState("");
   const [toDestination, setToDestination] = useState("");
   const [date, setDate] = useState("");
+
+  const [suggestions, setSuggestions] = useState("");
+  const [sourceSuggestion, setSourceSuggestion] = useState("");
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const useStyles = makeStyles((theme) => ({
@@ -33,6 +43,8 @@ const Search = () => {
   const classes = useStyles();
   const submitHandler = (event) => {
     event.preventDefault();
+    debugger;
+    console.log(date);
     let formattedDate = dateFormatter(date);
     dispatch(
       searchFlights({
@@ -43,61 +55,181 @@ const Search = () => {
     );
     navigate("/browse-page");
   };
-  const destinationChangeHandler = (e) => {
+
+  /**
+   * Destination change handler
+   * @param {*} e
+   * @returns {void}
+   */
+  const destinationChangeHandler = async (e) => {
     setToDestination(e.target.value);
+    let value = e.target.value;
+    const list = await axios.get(
+      "https://backendflym.herokuapp.com/api/getAll"
+    );
+    let items = [];
+    list.data.forEach((listItem) => {
+      items.push(listItem.name);
+    });
+
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, `i`);
+      setSuggestions(items.sort().filter((v) => regex.test(v)));
+    }
+    if (value.length === 0) {
+      setSuggestions("");
+    }
   };
-  const landmarkChangeHandler = (e) => {
+
+  /**
+   * Renders the suggestion for destination
+   * @returns
+   */
+  const renderDestinationSuggestions = () => {
+    if (suggestions.length === 0) {
+      return null;
+    }
+    return (
+      <ul>
+        {suggestions.map((city) => (
+          <li key={city} onClick={(e) => destinationSuggestionSelected(city)}>
+            {city}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  /**
+   * On Selection of destination suggestion.
+   * @param {*} value
+   */
+  const destinationSuggestionSelected = (value) => {
+    setToDestination(value);
+    setSuggestions("");
+  };
+
+  /**
+   * Chnage handler for landmark.
+   * @param {*} e
+   */
+  const sourceChangeHandler = async (e) => {
     setFromLandmark(e.target.value);
+    let value = e.target.value;
+    const list = await axios.get(
+      "https://backendflym.herokuapp.com/api/getAll"
+    );
+    let items = [];
+    list.data.forEach((listItem) => {
+      items.push(listItem.name);
+    });
+
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, `i`);
+      setSourceSuggestion(items.sort().filter((v) => regex.test(v)));
+    }
+    if (value.length === 0) {
+      setSourceSuggestion("");
+    }
   };
+
+  /**
+   * Renders the suggestion for source
+   * @returns
+   */
+  const renderSourceSuggestions = () => {
+    if (sourceSuggestion.length === 0) {
+      return null;
+    }
+    return (
+      <ul>
+        {sourceSuggestion.map((city) => (
+          <li key={city} onClick={(e) => sourceSuggestionSelected(city)}>
+            {city}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  /**
+   * On Selection of source suggestion.
+   * @param {*} value
+   */
+  const sourceSuggestionSelected = (value) => {
+    setFromLandmark(value);
+    setSourceSuggestion("");
+  };
+
+  /**
+   * Date change handler.
+   * @param {*} e
+   */
   const dateChangeHandler = (e) => {
     setDate(e.target.value);
   };
+
   return (
-    <div className={classes.root}>
-      <Grid container className={classes.centralised}>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-            <form className={classes['form']} onSubmit={submitHandler}>
-              <div className={classes["formfield"]}>
-                <label className="labels">To</label>
-                <input
-                  type="text"
-                  aria-label="ToDestination"
-                  aria-required="true"
-                  value={toDestination}
-                  onChange={destinationChangeHandler}
-                  required
-                ></input>
-              </div>
-              <div className={classes["formfield"]}>
-                <label className="labels">From</label>
-                <input
-                  type="text"
-                  aria-label="FromLandmark"
-                  value={fromLandmark}
-                  onChange={landmarkChangeHandler}
-                  aria-required="true"
-                  required
-                ></input>
-              </div>
-              <div className={classes["formfield"]}>
-                <label className="labels">Date</label>
-                <input
-                  type="date"
-                  aria-label="DateOfTravelling"
-                  value={date}
-                  onChange={dateChangeHandler}
-                  aria-required="true"
-                  required
-                ></input>
-              </div>
-              <Button color="primary" type="submit">
-                Submit
-              </Button>
-            </form>
-          </Paper>
+    <div className="search">
+      <div className={classes.root}>
+        <Grid container className={classes.centralised}>
+          <Grid item xs={1} md={6} xl={6}>
+            <img src={flying} className="home-image"></img>
+          </Grid>
+          <Grid item xs={11} md={6} xl={6} className="form-container">
+            <Paper className={classes.paper}>
+              <form className={classes["form"]} onSubmit={submitHandler}>
+                <div className={classes["formfield"]}>
+                  <label className="labels">To</label>
+                  <div className="TypeAheadDropDown">
+                    <input
+                      type="text"
+                      aria-label="ToDestination"
+                      aria-required="true"
+                      value={toDestination}
+                      onChange={destinationChangeHandler}
+                      required
+                    ></input>
+                    {renderDestinationSuggestions()}
+                  </div>
+                </div>
+                <div className={classes["formfield"]}>
+                  <label className="labels">From</label>
+                  <div className="TypeAheadDropDown">
+                    <input
+                      type="text"
+                      aria-label="FromLandmark"
+                      value={fromLandmark}
+                      onChange={sourceChangeHandler}
+                      aria-required="true"
+                      required
+                    ></input>
+                    {renderSourceSuggestions()}
+                  </div>
+                </div>
+                <div className={classes["formfield"]}>
+                  <label className="labels">Date</label>
+                  <div className="calender">
+                  <DatePicker selected={date} onChange={(date) => setDate(date)} />
+                  {/* <input
+                    type="date"
+                    aria-label="DateOfTravelling"
+                    value={date}
+                    onChange={dateChangeHandler}
+                    aria-required="true"
+                    className="datepicker-input"
+                    required
+                  ></input> */}
+                  </div>
+                </div>
+                <Button color="primary" type="submit">
+                  Submit
+                </Button>
+              </form>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </div>
   );
 };
